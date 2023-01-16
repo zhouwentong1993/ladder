c57b4cbabd7ffd8ccf536873f05a8ccbf01ea1c1
zhouwentong <zhouwentong@hualala.com>
2023-01-16 18:35:52 +0800

    学习扩展点设计

--- src/main/java/com/wentong/ladder/aviator/AviatorHelper.java
+++ src/main/java/com/wentong/ladder/aviator/AviatorHelper.java
@@ -1,9 +1,11 @@
 package com.wentong.ladder.aviator;
 
+import cn.hutool.core.lang.Dict;
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.AviatorEvaluatorInstance;
 import com.googlecode.aviator.Expression;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
@@ -16,7 +18,11 @@ public final class AviatorHelper {
     private AviatorHelper() {
     }
 
+    // 缓存编译结果，提升性能
     public static Function<String, Expression> COMPILE_FUNCTION = param -> INSTANCE.compile(param, true);
+
+    // 执行
+    public static BiFunction<String, Dict, Object> EXECUTE_FUNCTION = INSTANCE::execute;
 
 
 }
