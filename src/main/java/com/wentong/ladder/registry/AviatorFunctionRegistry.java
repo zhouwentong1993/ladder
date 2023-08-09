a9119bfbe52f7d6f4df585e064d1317f01bfd8c5
zhouwentong <zhouwentong@hualala.com>
2023-08-09 11:16:11 +0800

    自定义函数引入还有问题，待测试

--- src/main/java/com/wentong/ladder/registry/AviatorFunctionRegistry.java
+++ src/main/java/com/wentong/ladder/registry/AviatorFunctionRegistry.java
@@ -1,9 +1,11 @@
 package com.wentong.ladder.registry;
 
+import cn.hutool.extra.spring.SpringUtil;
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.spring.SpringContextFunctionLoader;
 import com.wentong.ladder.exceptions.AviatorFunctionException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Aviator 函数注册表，用来注册用户自定义函数
@@ -12,8 +14,10 @@ import lombok.extern.slf4j.Slf4j;
 public class AviatorFunctionRegistry {
 
     static {
+        ApplicationContext applicationContext = SpringUtil.getApplicationContext();
+
         // 提供 Spring 上下文支持，因为业务逻辑需要从 MySQL、Redis 等数据源获取，通常使用 Spring 方式。
-        AviatorEvaluator.addFunctionLoader(new SpringContextFunctionLoader());
+        AviatorEvaluator.addFunctionLoader(new SpringContextFunctionLoader(applicationContext));
     }
 
     public static void register(String name, Class clazz) {
--- src/test/java/com/wentong/ladder/scaner/JavaFunctionAnnotationScannerTest.java
+++ src/test/java/com/wentong/ladder/scaner/JavaFunctionAnnotationScannerTest.java
@@ -1,13 +1,22 @@
 package com.wentong.ladder.scaner;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.googlecode.aviator.AviatorEvaluator;
+import com.wentong.ladder.functions.vo.AddOrderParam;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class JavaFunctionAnnotationScannerTest {
 
     @Test
     void scan() {
         new JavaFunctionAnnotationScanner().scan("com.wentong.ladder");
+        AddOrderParam addOrderParam = new AddOrderParam();
+        addOrderParam.setPlatformType("PT365");
+        String expression = "prePriceFunctionDefinition.apply(addOrderParam)";
+        Object result = AviatorEvaluator.execute(expression, BeanUtil.beanToMap(addOrderParam));
+
+        System.out.println("Result: " + result);
     }
 }
