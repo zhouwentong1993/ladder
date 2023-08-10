0723283cbf64f14ca6714fba0e13fb4dd3b8513f
zhouwentong <zhouwentong@hualala.com>
2023-08-10 11:16:51 +0800

    自定义函数

--- src/main/java/com/wentong/ladder/entity/MetaHttpRequestEntity.java
+++ src/main/java/com/wentong/ladder/entity/MetaHttpRequestEntity.java
@@ -34,7 +34,34 @@ public class MetaHttpRequestEntity implements Serializable {
      * 所属业务，比如查询价格
      */
     private String refBusiness;
+
+    /**
+     * 所属的请求类
+     */
     private String refClass;
+
+    // --------------上面的是请求的字段----------------
+
+    /**
+     * 响应映射的类
+     */
+    private String responseClass;
+
+    /**
+     * 响应成功的判断，只有当响应成功后才会进行映射
+     */
+    private String responseSuccessJudgement;
+
+    /**
+     * 响应的 message 字段，通常用于错误提示
+     */
+    private String responseMessage;
+
+    /**
+     * 响应映射字段逻辑
+     */
+    private String responseMapping;
+
     /**
      * 创建时间
      */
--- src/main/java/com/wentong/ladder/functions/PrePriceFunction.java
+++ src/main/java/com/wentong/ladder/functions/PrePriceFunction.java
@@ -1,10 +1,14 @@
 package com.wentong.ladder.functions;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.extra.spring.SpringUtil;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.googlecode.aviator.runtime.function.FunctionUtils;
 import com.googlecode.aviator.runtime.type.AviatorObject;
+import com.googlecode.aviator.runtime.type.AviatorRuntimeJavaType;
 import com.wentong.ladder.annotations.JavaFunction;
+import com.wentong.ladder.aviator.AviatorHelper;
 import com.wentong.ladder.entity.MetaHttpRequestEntity;
 import com.wentong.ladder.exceptions.ClassInitializeException;
 import com.wentong.ladder.functions.vo.AddOrderParam;
@@ -31,10 +35,24 @@ public class PrePriceFunction extends LadderAbstractFunction {
         try {
             Object mapping = mappingHandler.mapping(addOrderParam, Class.forName(metaHttpRequestEntity.getRefClass()));
             Response response = HttpGetter.getResponse(metaHttpRequestEntity, mapping);
+            String responseSuccessJudgement = metaHttpRequestEntity.getResponseSuccessJudgement();
+            boolean successResponse = (boolean) AviatorHelper.COMPILED_FUNCTION.apply(responseSuccessJudgement).execute(BeanUtil.beanToMap(response));
+            // 是一个成功的响应，则对其进行映射
+            MappingHandler<JSONObject, Object> responseMappingHandler = new ClassMappingHandler<>();
+            Objects.requireNonNull(response.body(), "response body can not be null");
+            if (successResponse) {
+                // 这里暂时只处理 JSON 格式的响应结果
+                // 不对，这里是典型的一对多的映射，也就是每个平台都要映射到这一个对象，不能通过创建对象的模式来映射，应该通过语句映射，执行防范，返回一个 AviatorObject
+                Object responseMapping = AviatorHelper.COMPILED_FUNCTION.apply(metaHttpRequestEntity.getResponseMapping()).execute(JSONObject.parseObject(response.body().string()));
+                return AviatorRuntimeJavaType.valueOf(responseMapping);
+            } else { // 取 message 映射
+                // 因为报错通常有两种情况，一是 HTTP 级别的报错，比如 500。另外是业务报错，这时会是 JSON 结构。
+                Object messageMapping = AviatorHelper.COMPILED_FUNCTION.apply(metaHttpRequestEntity.getResponseMessage()).execute(Map.of("response", response));
+                return AviatorRuntimeJavaType.valueOf(messageMapping);
+            }
 
         } catch (Exception e) {
             throw new ClassInitializeException(e);
         }
-        return null;
     }
 }
