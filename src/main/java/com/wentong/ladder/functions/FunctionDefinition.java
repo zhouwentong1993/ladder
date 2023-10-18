b440c7c83c7a7afa21d798556036c9c9dd2bb55f
zhouwentong <zhouwentong@hualala.com>
2023-10-18 10:00:51 +0800

    增加注释

--- src/main/java/com/wentong/ladder/functions/FunctionDefinition.java
+++ src/main/java/com/wentong/ladder/functions/FunctionDefinition.java
@@ -3,8 +3,8 @@ package com.wentong.ladder.functions;
 /**
  * 如果需要自定义函数，需要实现该接口，并且在对象上使用 @JavaFunction 注解标注。
  * 注意：如果该自定义函数需要使用 Spring 上下文，需要使用 @Component 注解标注。
- * @param <S>
- * @param <T>
+ * @param <S> 函数入参
+ * @param <T> 函数返回值
  */
 public interface FunctionDefinition<S, T> {
 
